450953cb76dc1e22636ee9f212f83e7a1c100d77
Renom <93594802+maleyn@users.noreply.github.com>
2022-01-20 14:36:46 +0100

    Add files via upload

---
+++ blog.php
@@ -0,0 +1,154 @@
+<?php include('header.php'); ?>
+<main class="container marge-top">
+    <section id="blog" class="fondblog">
+        <h3>Actualités</h3>
+        <div id="actualite">
+            <article class="card" id="article1">
+                <a href="bloglien1.php"><img src="images/photo_instagram1_ld.jpg" alt="photo_instagram1"
+                        style="width:100%"></a>
+                <div class="card_container">
+                    <h4>02 Nov 2021 par John Doe</h4>
+                    <h5>Nouveau menu ! Venez découvrir</h5>
+                    <div class="lienblog">
+                        <a href="bloglien1.php">En savoir plus</a>
+                        <!-- bouton commentaire ouvre une boite de dialogue -->
+                        <label for="comment1">2 <i class="fas fa-comments"></i></label>
+                        <input id="comment1" type="checkbox" role="button" aria-pressed="true">
+                        <div class="boitecomment">
+                            <h6>John doe le 16/09/2021</h6>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                Molestiae eum ratione suscipit nisi sint officia omnis ullam nihil. Atque, quaerat!
+                            </p>
+                            <h6>John Doe le 14/09/2021</h6>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                Molestiae eum ratione suscipit nisi sint officia omnis ullam nihil. Atque, quaerat!
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </article>
+            <article class="card" id="article2">
+                <a href="bloglien2.php"><img src="images/photo_instagram2_ld.jpg" alt="photo_instagram2"
+                        style="width:100%"></a>
+                <div class="card_container">
+                    <h4>25 Oct 2021 par John Doe</h4>
+                    <h5>Un grand chef nous rend visite</h5>
+                    <div class="lienblog">
+                        <a href="bloglien2.php">En savoir plus</a>
+                        <label for="comment2">2 <i class="fas fa-comments"></i></label>
+                        <input id="comment2" type="checkbox" role="button" aria-pressed="true">
+                        <div class="boitecomment">
+                            <h6>John doe le 16/09/2021</h6>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                Molestiae eum ratione suscipit nisi sint officia omnis ullam nihil. Atque, quaerat!
+                            </p>
+                            <h6>John Doe le 14/09/2021</h6>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                Molestiae eum ratione suscipit nisi sint officia omnis ullam nihil. Atque, quaerat!
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </article>
+            <article class="card" id="article3">
+                <a href="bloglien3.php"><img src="images/photo_instagram3_ld.jpg" alt="photo_instagram3"
+                        style="width:100%"></a>
+                <div class="card_container">
+                    <h4>15 Oct 2021 par John Doe</h4>
+                    <h5>La carte change !</h5>
+                    <div class="lienblog">
+                        <a href="bloglien3.php">En savoir plus</a>
+                        <label for="comment3">2 <i class="fas fa-comments"></i></label>
+                        <input id="comment3" type="checkbox" role="button" aria-pressed="true">
+                        <div class="boitecomment">
+                            <h6>John doe le 16/09/2021</h6>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                Molestiae eum ratione suscipit nisi sint officia omnis ullam nihil. Atque, quaerat!
+                            </p>
+                            <h6>John Doe le 14/09/2021</h6>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                Molestiae eum ratione suscipit nisi sint officia omnis ullam nihil. Atque, quaerat!
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </article>
+            <article class="card" id="article4">
+                <a href="bloglien4.php"><img src="images/photo_instagram4_ld.jpg" alt="photo_instagram4"
+                        style="width:100%"></a>
+                <div class="card_container">
+                    <h4>3 Oct 2021 par John Doe</h4>
+                    <h5>Découvrez la nouvelle terrasse !</h5>
+                    <div class="lienblog">
+                        <a href="bloglien4.php">En savoir plus</a>
+                        <label for="comment4">2 <i class="fas fa-comments"></i></label>
+                        <input id="comment4" type="checkbox" role="button" aria-pressed="true">
+                        <div class="boitecomment">
+                            <h6>John doe le 16/09/2021</h6>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                Molestiae eum ratione suscipit nisi sint officia omnis ullam nihil. Atque, quaerat!
+                            </p>
+                            <h6>John Doe le 14/09/2021</h6>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                Molestiae eum ratione suscipit nisi sint officia omnis ullam nihil. Atque, quaerat!
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </article>
+            <article class="card" id="article5">
+                <a href="bloglien5.php"><img src="images/photo_instagram5_ld.jpg" alt="photo_instagram5"
+                        style="width:100%"></a>
+                <div class="card_container">
+                    <h4>15 Sep 2021 par John Doe</h4>
+                    <h5>Le site internet évolue !</h5>
+                    <div class="lienblog">
+                        <a href="bloglien5.php">En savoir plus</a>
+                        <label for="comment5">2 <i class="fas fa-comments"></i></label>
+                        <input id="comment5" type="checkbox" role="button" aria-pressed="true">
+                        <div class="boitecomment">
+                            <h6>John doe le 16/09/2021</h6>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                Molestiae eum ratione suscipit nisi sint officia omnis ullam nihil. Atque, quaerat!
+                            </p>
+                            <h6>John Doe le 14/09/2021</h6>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                Molestiae eum ratione suscipit nisi sint officia omnis ullam nihil. Atque, quaerat!
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </article>
+            <article class="card" id="article6">
+                <a href="bloglien6.php"><img src="images/photo_instagram6_ld.jpg" alt="photo_instagram6"
+                        style="width:100%"></a>
+                <div class="card_container">
+                    <h4>2 Sep 2021 par John Doe</h4>
+                    <h5>Le menu moussaillon en détails !</h5>
+                    <div class="lienblog">
+                        <a href="bloglien6.php">En savoir plus</a>
+                        <label for="comment6">2 <i class="fas fa-comments"></i></label>
+                        <input id="comment6" type="checkbox" role="button" aria-pressed="true">
+                        <div class="boitecomment">
+                            <h6>John doe le 16/09/2021</h6>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                Molestiae eum ratione suscipit nisi sint officia omnis ullam nihil. Atque, quaerat!
+                            </p>
+                            <h6>John Doe le 14/09/2021</h6>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                Molestiae eum ratione suscipit nisi sint officia omnis ullam nihil. Atque, quaerat!
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </article>
+        </div>
+        <!-- bouton de navigation du blog -->
+        <nav id="navigationblog">
+            <a class="boutonrond" href="#">1</a><span>-</span>
+            <a class="boutonrond" href="#">2</a><span>-</span>
+            <a class="boutonrond" href="#">3</a>
+        </nav>
+    </section>
+</main>
+<?php include('footer.php'); ?>
